1d0bfa1ed1e4ab75029be286ac057ca0b8d2735c
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2026-04-08 18:25:16 -0400

    removed config.yaml from Control Lane

--- src/packages/follow_lane/src/control_lane_node.py
+++ src/packages/follow_lane/src/control_lane_node.py
@@ -27,10 +27,7 @@ class ControlLaneNode:
 
         control_change_topic = f"/{self._vehicle_name}/switch/control"
         self.sub_control = rospy.Subscriber(control_change_topic, Int32, self.cbControl , queue_size = 1)
-        
-        #self.load_conf('/home/ubuntu/DuckieRace_2026/src/packages/follow_lane/config/detect_lane.yaml')
-        #self.sub_config = rospy.Subscriber(f"/{self._vehicle_name}/conf", String, self.cbUpdateConf, queue_size = 1)
-        
+ 
         self.lastError = 0
         self.v = 0
         self.a = 0
@@ -54,33 +51,17 @@ class ControlLaneNode:
         else:
             self.enable = False
 
-    def cbFollowLane(self, desired_center):
+    # error between 1 and -1
+    def cbFollowLane(self, error):
 
         print(f'received message. enabled : {self.enable}')
-
         if not self.enable:
             return        
-        
-        center = desired_center.data
-        self.followLane(center)
+        error = error.data
 
-    
-    # error between 1 and -1
-    def followLane(self, error):
-        error = -error
-
-        Kp = self.kp
-        Kd = self.kd
-
-        print(f'error {error}')
-
-        a = -(Kp * error + Kd * (error - self.lastError) )
-        self.lastError = error
-        
-        # twist.linear.x = 0.05        
-        v = min(self.MAX_VEL*100 * ((1 - abs(error)) ** 2), self.MAX_VEL)
-        self.v = v
-        self.a = a                
+        #Todo Write own code for PID controller here
+        self.v = 0
+        self.a = 0                
         
 
     def fnShutDown(self):
@@ -88,21 +69,6 @@ class ControlLaneNode:
 
         twist = Twist2DStamped(v=0.0, omega=0.0)
         self.pub_cmd_vel.publish(twist) 
-
-    def load_conf(self,path):
-        with open(path,'r') as f:
-            conf_yml = f.read()
-        
-        self.update_conf(conf_yml)
-    def cbUpdateConf(self,conf_msg):
-        self.update_conf(conf_msg.data)
-    
-    def update_conf(self,conf_yml):
-        self.conf = yaml.safe_load(conf_yml)
-        self.MAX_VEL = self.conf['pid_regler']['max_vel'] /100
-        self.kp = self.conf['pid_regler']['p'] /10
-        self.kd = self.conf['pid_regler']['d'] /10
-        self.ki = self.conf['pid_regler']['i'] /10
 
     def run(self):
         rate = rospy.Rate(10)
@@ -114,12 +80,6 @@ class ControlLaneNode:
             twist.v = self.v
             twist.omega = self.a
 
-            #print(f'type: {type(self.pub_cmd_vel)}')
-            #print(f'topic: {self.pub_cmd_vel.name}')
-            #print(f'message type: {self.pub_cmd_vel.data_class} actualy type{type(twist)}')
-            #print(f'max Vel: {self.MAX_VEL}')
-            #print(f'pid {self.kp}, {self.ki}, {self.kd}')
-            #print(f'message was: {twist}')
             self.pub_cmd_vel.publish(twist)
 
             rate.sleep()
